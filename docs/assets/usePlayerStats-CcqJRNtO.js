5cdb495e564d1f7660931c1507d962250a04f9c5
Pierre Gondolff <pierre.gondolff@cyncly.com>
2025-08-15 16:35:16 +0200

    Merge CampAverage and CampVictory into one component, Camps

---
+++ docs/assets/usePlayerStats-CcqJRNtO.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-_KzV5mQ9.js";function n(){const{combinedData:t,isLoading:a,error:r}=e();return{playerStatsData:t&&t.playerStats?t.playerStats:null,dataLoading:a,fetchError:r}}export{n as u};
