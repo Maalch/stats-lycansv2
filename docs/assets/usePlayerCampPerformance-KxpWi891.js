02d9fb78d93d557dd64ed3e2259a5510e1ae2eb2
Pierre Gondolff <pierre.gondolff@cyncly.com>
2025-08-15 23:25:24 +0200

    Build for website

--- docs/assets/usePlayerCampPerformance-KxpWi891.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index-BMzbn9yu.js";function p(){const{combinedData:r,isLoading:e,error:a}=o();return{playerCampPerformance:r&&r.playerCampPerformance?r.playerCampPerformance:null,isLoading:e,error:a}}export{p as u};
